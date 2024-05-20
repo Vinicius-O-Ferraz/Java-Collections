ccb00a1b629e447eb3f59b54f95a1669158e2dc2
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-05-20 14:41:37 -0300

    Refractor: class name change

---
+++ lists/src/TaskInterface.java
@@ -0,0 +1,7 @@
+public interface Task {
+
+    public void addTask(String description);
+    public void removeTask(String description);
+    public int getNumberOfTasks();
+    public String getDescriptionOfTasks();
+}
