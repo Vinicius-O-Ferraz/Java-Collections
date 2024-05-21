f90ac9558be0b0fa9bd4bf65d580302d6e76a8bc
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-05-21 10:34:46 -0300

    feat: create java book class and implemented getters and setters

---
+++ lists/src/book/Book.java
@@ -0,0 +1,37 @@
+package book;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year_of_publication;
+
+    public Book(String author, String title, int year_of_publication) {
+        this.author = author;
+        this.title = title;
+        this.year_of_publication = year_of_publication;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear_of_publication() {
+        return year_of_publication;
+    }
+
+    public void setYear_of_publication(int year_of_publication) {
+        this.year_of_publication = year_of_publication;
+    }
+}
